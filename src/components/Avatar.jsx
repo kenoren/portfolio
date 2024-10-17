a640b011a889eb9b79722ddbde561f1f48d509e3
Mathis Fourey <mathisfou@gmail.com>
2024-10-17 20:32:13 +0200

    ajout animation

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -4,16 +4,66 @@ Command: npx gltfjsx@6.5.2 public/models/670ec6c584bf15b4adaef5a0.glb
 */
 
 import React from 'react'
-import { useGraph } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { useFrame, useGraph } from '@react-three/fiber'
+import { useAnimations, useFBX, useGLTF } from '@react-three/drei'
 import { SkeletonUtils } from 'three-stdlib'
+import { useEffect, useRef } from 'react';
+import { useControls } from 'leva';
+import * as THREE from 'three';
+
+
+//import { MathUtils } from 'three';
+
 
 export function Avatar(props) {
+  const {animation} = props;
+  const {headFollow, cursorFollow, wireframe} = useControls({
+    headFollow: false,
+    cursorFollow: false,
+    wireframe: false
+  });
+
+  const group = useRef();
   const { scene } = useGLTF('models/670ec6c584bf15b4adaef5a0.glb')
+
+  const {animations: typingAnimation} = useFBX("animations/Typing.fbx");
+  const {animations: standingAnimation} = useFBX("animations/Standing Idle.fbx");
+  const {animations: fallingAnimation} = useFBX("animations/Falling Idle.fbx");
+
+  typingAnimation[0].name = "Typing";
+  standingAnimation[0].name = "Standing";
+  fallingAnimation[0].name = "Falling";
+
+  const{actions} = useAnimations([typingAnimation[0],standingAnimation[0],fallingAnimation[0]], group);
+
+  useFrame((state) => {
+    if (headFollow){
+      group.current.getObjectByName("Head").lookAt(state.camera.position);
+    }
+    if (cursorFollow){
+      const target = new THREE.vector3(state.mouse.x, state.mouse.y, 1);
+      group.current.getObjectByName("Spine2").lookAt(target);
+    }
+  });
+
+  useEffect(() => {
+    actions[animation].reset().fadeIn(0.5).play();
+    return () => {
+      actions[animation].reset().fadeOut(0.5);
+    }
+  }, [animation]);
+
+  useEffect(() => {
+    Object.values(materials).forEach((material) => {
+      material.wireframe = wireframe
+    })
+  }, [wireframe]);
+
   const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene])
   const { nodes, materials } = useGraph(clone)
   return (
-    <group {...props} dispose={null}>
+    <group {...props} ref={group} dispose={null}>
+      <group rotation-x={-Math.PI / 2}>
       <primitive object={nodes.Hips} />
       <skinnedMesh geometry={nodes.Wolf3D_Hair.geometry} material={materials.Wolf3D_Hair} skeleton={nodes.Wolf3D_Hair.skeleton} />
       <skinnedMesh geometry={nodes.Wolf3D_Glasses.geometry} material={materials.Wolf3D_Glasses} skeleton={nodes.Wolf3D_Glasses.skeleton} />
@@ -25,6 +75,7 @@ export function Avatar(props) {
       <skinnedMesh name="EyeRight" geometry={nodes.EyeRight.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeRight.skeleton} morphTargetDictionary={nodes.EyeRight.morphTargetDictionary} morphTargetInfluences={nodes.EyeRight.morphTargetInfluences} />
       <skinnedMesh name="Wolf3D_Head" geometry={nodes.Wolf3D_Head.geometry} material={materials.Wolf3D_Skin} skeleton={nodes.Wolf3D_Head.skeleton} morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences} />
       <skinnedMesh name="Wolf3D_Teeth" geometry={nodes.Wolf3D_Teeth.geometry} material={materials.Wolf3D_Teeth} skeleton={nodes.Wolf3D_Teeth.skeleton} morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences} />
+    </group>
     </group>
   )
 }
